f821f6580f7e7bfd92bcd2d47741e150565d1802
Podnebesnii <gidronya@gmail.com>
2022-06-08 19:13:46 +0300

    Убрал maxresults

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -32,7 +32,7 @@ public class UserDaoImp implements UserDao {
       String hql = "from User user where user.car.model = :model and user.car.series = :series";
       TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
       query.setParameter("model", model).setParameter("series", series);
-      return query.setMaxResults(1).getSingleResult();
+      return query.getSingleResult();
    }
 
 
